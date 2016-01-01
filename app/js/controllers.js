18658ee93829b9108eeebf5d04268403a7865573
danagilliann <danalee96@gmail.com>
2016-01-02 01:12:00 +0800

    switched all content to angular

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,19 +5,26 @@
   
   /* Controller of index/homepage */
   demodays.controller('IndexCtrl', function($scope) { 
+    
     /* Commonly used */
-    $scope.RSVP = "RSVP";
-    $scope.FBEvent = "https://www.facebook.com/events/389260677946329/";
+    var RSVP = "RSVP";
+    var RSVPForm = "https://www.facebook.com/events/389260677946329/";
+    var demoForm = "https://docs.google.com/forms/d/1pAs9qC2fjLJ2EUA6Fr-vZJ9gY7sZFhk0Zwr4qDphQEE/viewform";
+
+    $scope.RSVP = RSVP;
+    $scope.RSVPForm = RSVPForm;
 
     /* Title row */
-    $scope.title = "Demodays";
+    $scope.title = "DemoDays";
     $scope.tagline = "Student demos at the intersections of art, design, and code";
-  
+    $scope.image = "";
+
     /* Info in box */
     $scope.date = "6/11/2015";
+    $scope.monthYear = "Jun 2015";
     $scope.day = "Sunday";
     $scope.time = "2:00-6:00";
-    $scope.detailTime = "<li>Hacking</li><li>Demos</li><li>Keynote</li><li>Fooooood</li>";
+    $scope.detailTime = "<li>Hacking</li><li>Demos</li><li>Keynote</li><li>Fooooood</li>"; // sanitize
     $scope.location = "Pfizer Auditorium, NYU Poly";
     $scope.address = "<li>5 MetroTech Center 1st Floor, Dibner Building</li><li>Brooklyn, NY 11201</li>";
 
@@ -26,18 +33,20 @@
     $scope.currentDesc = "Join us for a special Demo Days and Keynote Speaker as part of <a href=\"http://nyusw.com/\">Startup Week</a>, in partnership with <a href=\"http://hacknyu.org/\">Hack NYU</a>!"; // sanitize
     
     /* Show off & hang out row */
-    $scope.showOff = [
+    $scope.showHangs = [
       { 
         icon: "ion-ios7-lightbulb",
         title: "Wanna show off?",
         subtitle: "SIGN UP",
-        href: "https://docs.google.com/forms/d/1pAs9qC2fjLJ2EUA6Fr-vZJ9gY7sZFhk0Zwr4qDphQEE/viewform"
+        href: demoForm,
+        cls: "demo"
       },
       {
         icon: "ion-ios7-glasses",
         title: "Wanna hang out?",
         subtitle: "RSVP",
-        href: "https://www.facebook.com/events/389260677946329/"
+        href: RSVPForm,
+        cls: "attend"
       }
     ];
 
@@ -47,31 +56,39 @@
 
     /* Current program row */
     $scope.programTitle = "April 12th Program";
-    $scope.programDesc = "<p>2:00 - 2:45 Refreshments</p><p>3:00 - 4:00 Surprise keynote speaker</p><p>4:00 - 5:30 Student demos!</p><p>See <a href=\"http://hacknyu.org/#schedule\">HackNYU</a> for the hackathon leading up to this event.</p>";
+    $scope.programDesc = [
+      "2:00 - 2:45 Refreshments",
+      "3:00 - 4:00 Surprise keynote speaker", 
+      "4:00 - 5:30 Student demos!", 
+      "See <a href=\"http://hacknyu.org/#schedule\">HackNYU</a> for the hackathon leading up to this event.</p>"
+    ]; // sanitize
 
     /* Keynote speakers row */
     $scope.keynoteTitle = "Keynote Speaker";
     $scope.keynoteSpeakers = [
       {
-        img: "assets/img/hilary-mason.jpg",
+        img: "../assets/img/hilary-mason.jpg",
+        name: "Hilary Mason",
         desc: "<a href=\"https://www.twitter.com/hmason\">Hilary Mason</a>, founder of <a href=\"http://www.fastforwardlabs.com/\">Fast Forward Labs</a> and former Data Scientist at <a href=\"https://accel.com\">Accel</a> and <a href=\"https://bitly.com/\">Bit.ly</a>. Cofounder of <a href=\"http://hackny.org/\">HackNY</a>." // sanitize
       }
     ];
 
     /* Sign up for demos row */
     $scope.signupTitle = "Demo Program";
-    $scope.signupDesc = "Want to demo your project? Sign up <a href=\"https://docs.google.com/forms/d/1pAs9qC2fjLJ2EUA6Fr-vZJ9gY7sZFhk0Zwr4qDphQEE/viewform\">here</a>!";
+    $scope.signupDesc = "Want to demo your project? Sign up <a href=\"" + demoForm + "\">here</a>!"; // sanitize
 
     /* Hosts row */
     $scope.hostsTitle = "Hosts";
     $scope.hostsImg = [
       {
-        src: "assets/logos/techatnyu.png",
-        alt: "tech@nyu"
+        src: "../assets/logos/techatnyu.png",
+        name: "tech@nyu",
+        href: "http://techatnyu.org"
       },
       { 
-        src: "assets/logos/nyuscc.png",
-        alt: "hacknyu"
+        src: "../assets/logos/nyuscc.png",
+        name: "hacknyu",
+        href: "http://hacknyu.org"
       }
     ];
 
@@ -80,60 +97,91 @@
     $scope.prevSponsorsImg = [ 
       { 
         href: "http://createatcooper.org/",
-        hrefTitle: "create@cooper",
-        src: "assets/logos/cooper.png",
+        title: "create@cooper",
+        src: "../assets/logos/cooper.png",
         alt: "create@cooper"
       },
       {
         href: "https://www.facebook.com/Eianyupoly",
-        hrefTitle: "EIA",
-        src: "assets/logos/eia.png",
+        title: "EIA",
+        src: "../assets/logos/eia.png",
         alt: "NYU Poly Entrepreneurship and Innovation Association"
       },
       { 
         href: "http://localhackday.mlh.io",
-        hrefTitle: "Local Hack Day",
-        src: "assets/logos/mlh.png",
+        title: "Local Hack Day",
+        src: "../assets/logos/mlh.png",
         alt: "Major League Hacking"
       },
       {
         href: "http://spotify.com",
-        hrefTitle: "Spotify",
-        src: "assets/logos/spotify.png",
+        title: "Spotify",
+        src: "../assets/logos/spotify.png",
         alt: "Spotify"
       },
       {
         href: "http://quirky.com",
-        hrefTitle: "Quirky",
-        src: "assets/logos/quirky.png",
+        title: "Quirky",
+        src: "../assets/logos/quirky.png",
         alt: "Quirky"
       },
       {
         href: "http://squarespace.com",
-        hrefTitle: "Squarespace",
-        src: "assets/logos/squarespace.png",
+        title: "Squarespace",
+        src: "../assets/logos/squarespace.png",
         alt: "Squarespace"
       },
       {
         href: "https://www.catercow.com/",
-        hrefTitle: "CaterCow",
-        src: "assets/logos/catercow.png",
+        title: "CaterCow",
+        src: "../assets/logos/catercow.png",
         alt: "CaterCow"
       },
       {
         href: "https://www.facebook.com/parsonscodeclub",
-        hrefTitle: "Parsons Code Club",
-        src: "assets/logos/pcc.png",
+        title: "Parsons Code Club",
+        src: "../assets/logos/pcc.png",
         alt: "Parsons Code Club"
       },
       {
         href: "http://adicu.com/",
-        hrefTitle: "Columbia Application Development Initiative",
-        src: "assets/logos/adi.png",
+        title: "Columbia Application Development Initiative",
+        src: "../assets/logos/adi.png",
         alt: "Columbia ADI"
       }
     ]
 
+    /* Footer */
+    $scope.footerTitle = "DemoDays";
+    $scope.footerDesc = "<a href=\"http://techatnyu.org\">tech@NYU</a> in conjunction with <a href=\"http://hacknyu.org/\">Hack NYU</a>.Want to attend more events like this? <a href=\"http://techatnyu.org\">Learn more about tech@NYU</a>!</p>"; // sanitize
+    $scope.footerSocial = [
+      { 
+        href: "https://www.facebook.com/TechatNYU",
+        icon: "ion-social-facebook",
+        title: "facebook"
+      },
+      {
+        href: "https://twitter.com/TechatNYU",
+        icon: "ion-social-twitter",
+        title: "twitter"
+      },
+      {
+        href: "mailto:hello@techatnyu.org",
+        icon: "ion-ios7-email",
+        title: "email"
+      }
+    ];
+    $scope.footerSignature = "made with <i class=\"ion-ios7-heart-outline\"><span>love</span></i> by";
+    $scope.footerAuthors = [ 
+      {
+        name: "Cheryl Wu, ",
+        link: "http://grungerabbit.com"
+      },
+      {
+        name: "Dana Lee",
+        link: "http://danagilliann.me"
+      }
+    ];
   });
 
 })();
---
+++ app/js/index.js
@@ -0,0 +1,135 @@
+(function ($) {
+  var DD = {
+    'config' : {
+    },
+    'init' : function () {
+      $(".further-detail").hover( function() {
+        $(this).children(".micro").toggle();
+      });
+      
+      var ar = DD.archive();
+      ar.setup();
+
+        //$(document).scroll(function() {
+      //  ar.navigate();
+        //});
+      
+        $(".archive li").on( "click mouseover mouseout", function() {
+        var arSpecial = DD.archive($(this));
+        
+        if (event.type === "click") {
+          arSpecial.past();
+        } 
+      });
+    },
+    'archive' : function (element) {
+        var windowTest = $(window).width();
+        var boxWidth = $(".archive__wrap").outerWidth();
+        var arcWidth = windowTest - boxWidth;
+        var $archive = $(".archive");
+      var numEntries = $archive.find("li").length;
+      
+          var top = $(document).scrollTop();
+      var page = $(window).height();
+      var doc = $("body").outerHeight();
+          var $month = $(".archive li");
+          var headerHeight = $("header").height() / 2;
+      var bottom = top+page;
+      
+      function archiveWidth() {
+        if (windowTest < 650) {
+          $archive.remove();
+          return;
+        } else {
+          $archive.width(windowTest);
+          $archive.find("li").width((windowTest/numEntries));
+        }
+      }
+      
+      function pastFader() {
+          $archive.find("a").each(function() {
+              var index = $(this).index();
+    
+              var colorWrap = "rgba(0,0,0,";
+              var paren = ")";
+              var testOpacity = 1 - (index * .1);
+              var test = colorWrap + testOpacity + paren;
+        
+              $(this).find("div").css("color", test);
+          });
+      }
+      
+      function currentArrow() {
+         $(".archive li.active").find(".past__arrow").show();
+      }
+      
+      function chooseMonth() {
+            var cinderella = element;
+            var stepsisters = element.siblings("li");
+      
+            cinderella.addClass("active");
+        cinderella.find("div").toggleClass("active");
+        cinderella.find(".past__arrow").show();
+            stepsisters.removeClass("active");
+        stepsisters.find(".past__date").removeClass("active");
+        stepsisters.find(".past__arrow").hide();        
+      }
+      
+      function fadeArchive() {
+        // TO DO: finish/throttle this
+          
+        console.log("is this on")
+        
+        if (top < headerHeight || bottom >= doc) {
+          $month.find(".past__date").fadeIn();
+        } else {
+          $month.find(".past__date").fadeOut();    
+        }
+        
+        //$(".archive li").hover( function() {
+        //  return;
+        //}, function() {
+        //  return;
+        //})   
+      }
+      
+      function showMonth() {
+            if ($(document).scrollTop() > 380)  {
+                element.find(".past__date").fadeIn();
+            }
+      }
+      
+      function hideMonth() {
+            if ($(document).scrollTop() > 380) {
+                element.find(".past__date").fadeOut();
+          }
+      }
+
+      return {
+        'setup' : function () {
+          archiveWidth();
+          pastFader();
+          currentArrow();
+        },
+        'past' : function () {
+          chooseMonth();
+        },
+        'navigate' : function () {
+          fadeArchive();
+        },
+        'showMonth' : function () {
+          showMonth();
+        },
+        'hideMonth' : function () {
+          hideMonth();
+        }
+      }
+    }
+  }
+  
+  $(document).ready(function() {
+    DD.init();
+  });
+  
+}(jQuery));
+
