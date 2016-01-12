1c0af8f44354a4596acc3e1c56421695666207f3
danagilliann <danalee96@gmail.com>
2016-01-12 12:40:19 +0800

    finished parsing date, monthYear, and demoUrl

--- app/scripts/process.py
+++ app/scripts/process.py
@@ -1,7 +1,7 @@
 # These two commands solve issue of HTTPS Insecure Platform in Python:
 # pip install requests
-
-import requests, json, os, unicodedata
+from bs4 import BeautifulSoup
+import requests, json, os, unicodedata, datetime
 
 headers = {
     'content-type': 'application/vnd.api+json',
@@ -10,12 +10,28 @@ headers = {
 r = requests.get('https://api.tnyu.org/v3/events?sort=-startDateTime&filter[simple][teams]=53f99d48c66b44cf6f8f6d81', headers=headers, verify=False)
 data = json.loads(r.text)
 
-details = ""
-
 demo = data['data'][0]
 title = demo["attributes"].get("title")
 title = unicodedata.normalize('NFKD', title).encode('ascii','ignore')
 rsvpUrl = demo["attributes"].get("rsvpUrl")
-details = demo["attributes"].get("details")
 
-# to do: parse through details
+# date and monthYear parsing
+startDateTime = demo["attributes"].get("startDateTime")
+startDateTime = datetime.datetime.strptime(startDateTime, "%Y-%m-%dT%H:%M:%S.%fZ")
+monthYear = startDateTime.strftime("%b %Y")
+startDateTime = startDateTime.timetuple()
+date = ""
+for a in range(2,-1,-1):
+    if a != 0:
+        date += str(startDateTime[a]) + "/"
+    elif a == 0:
+        date += str(startDateTime[a])
+
+# demoUrl parsing from details
+demoUrl = ""
+details = demo["attributes"].get("details")
+details = unicodedata.normalize('NFKD', details).encode('ascii','ignore')
+details_soup = BeautifulSoup(details, 'html.parser')
+for link in details_soup.find_all('a'):
+    if "goo.gl/forms" in link.string:
+        demoUrl = link.string
